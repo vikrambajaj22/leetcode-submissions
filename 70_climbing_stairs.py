495f6435e39153a8b3badb3b0709f2a9544a1fe9
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-09-23 17:42:07 -0400

    update comment

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # will be sum of fibonacci series till n
+        # will be n-1'th element of fibonacci series
         fib = [1, 2]  # when n=1, #ways=1; when n=1, #ways=2
         for i in range(2, n):
             fib.append(fib[i-1] + fib[i-2])
