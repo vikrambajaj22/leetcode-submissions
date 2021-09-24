e20f8e563446e977df605b9be9cc9407c0a44dd4
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-09-24 19:26:55 -0400

    update submission

--- 338_counting_bits.py
+++ 338_counting_bits.py
@@ -16,3 +16,28 @@ class Solution(object):
             ans.append(self.num_one_bits(i))
 
         return ans
+
+
+# alternate solution using dynamic programming
+class Solution2(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0]
+
+        # logic: any even number x will have the same number of 1s as x/2,
+        # since multiplying with 2 just adds a 0 at the end;
+        # ex. 2->10, 4->100, 8->1000
+        # any odd number x will have one more 1 than x/2
+        # ex. 2->10, 5->101; 3->11, 7->111
+
+        # using dynamic programming
+        for i in range(1, n+1):
+            if i % 2 == 0:
+                ans.append(ans[i/2])
+            else:
+                ans.append(ans[i/2] + 1)
+
+        return ans
