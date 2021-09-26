bca00569a144697e0d2bd6bd46e6396101c829cf
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-09-26 19:30:28 -0400

    add more submissions

---
+++ 160_intersection_of_two_lists.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        a = headA
+        b = headB
+
+        while a != b:
+            a = a.next if a else headB
+            b = b.next if b else headA
+
+        return a
