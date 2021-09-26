f2a3b4c8a433fb3f2f700468497b02a3fefabf6c
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-09-26 19:35:24 -0400

    add comment

--- 160_intersection_of_two_lists.py
+++ 160_intersection_of_two_lists.py
@@ -13,6 +13,10 @@ class Solution(object):
         a = headA
         b = headB
 
+        # logic: traverse to the end of A and B, if either of them reaches the end first, there is a difference of length
+        # by reassigning the pointer to the head of the other list, this difference in lengths is accounted for
+        # so, the pointers will either meet in the next traversal or both reach end at the same time (if there is no intersection)
+
         while a != b:
             a = a.next if a else headB
             b = b.next if b else headA
