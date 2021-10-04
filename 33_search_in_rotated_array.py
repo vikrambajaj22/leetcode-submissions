713d2faa9797e76ff5a39d931427556bb957146a
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-10-04 19:17:43 -0400

    update submission

--- 33_search_in_rotated_array.py
+++ 33_search_in_rotated_array.py
@@ -1,4 +1,19 @@
 class Solution(object):
+    def binary_search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+
     def search(self, nums, target):
         """
         :type nums: List[int]
@@ -6,46 +21,29 @@ class Solution(object):
         :rtype: int
         """
         if len(nums) == 1:
-            return -1 if nums[0] != target else 0
+            return 0 if nums[0] == target else -1
 
-        # find split index i.e. index at which order changes
-        split_index = -1
+        change_index = -1
         for i in range(len(nums)-1):
             if nums[i] > nums[i+1]:
-                split_index = i
+                change_index = i
                 break
 
-        found = False
+        if change_index != -1:
+            change = nums[change_index]
 
-        if split_index > -1:
-            if target > nums[-1] and target <= nums[split_index]:
-                # look from 0 to split_index
-                for i in range(0, split_index+1):
-                    if nums[i] == target:
-                        found = True
-                        return i
-            elif target >= nums[split_index+1] and target <= nums[-1]:
-                # look from split_index+1 to end
-                for i in range(split_index+1, len(nums)):
-                    if nums[i] == target:
-                        found = True
-                        return i
-            else:
+            if target > change:
                 return -1
-        else:
-            # binary search since it is ordered
-            low = 0
-            high = len(nums) - 1
 
-            while low <= high:
-                mid = (high + low) // 2
-                if target > nums[mid]:
-                    low = mid + 1
-                elif target < nums[mid]:
-                    high = mid - 1
+            if nums[0] <= target <= change:
+                return self.binary_search(nums[:change_index+1], target)
+            else:
+                index = self.binary_search(nums[change_index+1:], target)
+                if index != -1:
+                    return (change_index+1) + index
                 else:
-                    return mid
-            return -1
+                    return index
 
-        if not found:
-            return -1
+        else:
+            # perform normal binary search since there was no rotation
+            return self.binary_search(nums, target)
