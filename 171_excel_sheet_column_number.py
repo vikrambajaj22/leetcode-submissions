70d30b58304552da4b509d11c492ad73102243d4
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-10-07 19:39:30 -0400

    update submission

--- 171_excel_sheet_column_number.py
+++ 171_excel_sheet_column_number.py
@@ -4,15 +4,19 @@ class Solution(object):
         :type columnTitle: str
         :rtype: int
         """
-        alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        i = 1
-        d = {}
-        for a in alpha:
-            d[a] = i
-            i += 1
+        letters = list(columnTitle)
 
-        res = 0
-        for a in columnTitle:
-            res = res * 26 + d[a]
+        values = {k: i+1 for i, k in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ')}
 
-        return res
+        columnValue = 0
+
+        i = len(letters) - 1
+
+        while i >= 0:
+            columnValue += 26**(len(letters)-1-i) * values[letters[i]]
+            i -= 1
+
+        # logic: AB = 26^1 * A + 26^0 * B = 28; ZY = 26^1 * Z + 26^0 * Y = 701
+        # i.e. same as binary to decimal but base 26
+
+        return columnValue
