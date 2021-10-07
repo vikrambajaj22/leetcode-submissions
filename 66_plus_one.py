2db7af1587eed02cc1c4f684dd52c13c2e173544
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-10-07 00:25:46 -0400

    update submission

--- 66_plus_one.py
+++ 66_plus_one.py
@@ -4,33 +4,24 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        solution = []
-
-        last_digit = digits[-1]
-
-        if last_digit <= 8:
-            digits[-1] = last_digit + 1
+        if digits[-1] < 9:
+            digits[-1] += 1
             return digits
-        else:
-            # last digit was 9
-            carry_one = True
-            i = len(digits) - 1
-            answer = []
-            while carry_one and i >= 0:
-                if digits[i] == 9:
-                    carry_one = True
-                    answer.append(0)
-                else:
+
+        carry_one = True
+        i = len(digits) - 1
+        while i >= 0 and carry_one:
+            if digits[i] == 9:
+                carry_one = True
+                digits[i] = 0
+            else:
+                if carry_one:
                     digits[i] += 1
                     carry_one = False
-                    break
-                i -= 1
+            i -= 1
 
-            if carry_one:
-                # reached beggining (i<0) and still carry one, ex. 999
-                answer = [1] + answer
-            elif i >= 0:
-                # no more carry one, might not have reached beginning i.e. i may be >= 0
-                answer = digits[:i+1] + answer
+        if carry_one:
+            # reached i=-1 but carry_one is True
+            return [1] + digits
 
-            return answer
+        return digits
