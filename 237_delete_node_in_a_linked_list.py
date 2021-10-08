723089b66aebaf965b880238626c34e878f06dfd
Vikram Bajaj <vikrambajaj220496@gmail.com>
2021-10-07 20:21:53 -0400

    update submission with comment

--- 237_delete_node_in_a_linked_list.py
+++ 237_delete_node_in_a_linked_list.py
@@ -10,5 +10,7 @@ class Solution(object):
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        # cannot simply do node = node.next because prev node in the list still points to the old reference 'node'
+        # instead, copy over value of the next node into current 'node' and skip over the next node
         node.val = node.next.val  # since node isn't the tail (last node)
         node.next = node.next.next
