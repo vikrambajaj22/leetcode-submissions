a3dd8c3e215d8a7f17fbbf342bf2fda4d0c494c7
Vikram Bajaj <vikrambajaj220496@gmail.com>
2022-02-01 00:57:52 -0500

    add submission

--- 198_house_robber.py
+++ 198_house_robber.py
@@ -7,12 +7,12 @@ class Solution(object):
         # dynamic programming: at every step, choose max of (curr_house+2_houses_before) and prev_house
         if len(nums) == 1:
             return nums[0]
-        
+
         two_before, prev = nums[0], max(nums[0], nums[1])
-        
+
         for i in range(2, len(nums)):
             curr_max = max(nums[i]+two_before, prev)
             two_before = prev
             prev = curr_max
-            
+
         return prev  # stores the accumulated max
