4962cbf408a967aed13e4143d9fef4d69e75da63
Vikram Bajaj <vikrambajaj220496@gmail.com>
2022-03-19 20:13:59 -0400

    updte submission

--- 414_third_maximum_number.py
+++ 414_third_maximum_number.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        unique = list(set(nums))
-        if len(unique) < 3:
-            return max(unique)
-        return sorted(unique)[len(unique)-3]
+        # # non-heap solution
+        # unique = list(set(nums))
+        # if len(unique) < 3:
+        #     return max(unique)
+        # return sorted(unique)[len(unique)-3]
+
+        # heap solution
+        nums = list(set(nums))
+
+        heap = []
+
+        for n in nums:
+            if len(heap) == 3:
+                if n > heap[0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, n)
+            else:
+                heapq.heappush(heap, n)
+
+        if len(heap) < 3:
+            return heap[-1]
+        else:
+            return heap[0]
