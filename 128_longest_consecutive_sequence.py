cc6c2d6708a3c1b8e15875bd1349fd219c308297
Vikram Bajaj <vikrambajaj220496@gmail.com>
2022-04-06 19:29:14 -0400

    simplify solution

--- 128_longest_consecutive_sequence.py
+++ 128_longest_consecutive_sequence.py
@@ -26,11 +26,10 @@ class Solution(object):
         max_len = 0
 
         for num in unique:
-            if num-1 in unique:
-                continue
-            curr_len = 1
-            while num + curr_len in unique:
-                curr_len += 1
-            max_len = max(max_len, curr_len)
+            if num-1 not in unique:
+                curr_len = 1
+                while num + curr_len in unique:
+                    curr_len += 1
+                max_len = max(max_len, curr_len)
 
         return max_len
