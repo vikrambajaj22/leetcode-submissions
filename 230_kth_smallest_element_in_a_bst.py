cd1290802e4d5d9a11e590ac52be6201bf34b62b
Vikram Bajaj <vikrambajaj@nyu.edu>
2022-07-28 23:58:27 -0400

    add solution

--- 230_kth_smallest_element_in_a_bst.py
+++ 230_kth_smallest_element_in_a_bst.py
@@ -26,3 +26,36 @@ class Solution(object):
         if root.right:
             self.inorder(root.right, traversal)
             
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class AlternateSolution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        # BST inorder traversal is in ascending order
+        # performing inorder traversal till k nodes are traversed
+        
+        self.res = None
+        self.k = k
+        self.inorder(root)
+        return self.res
+        
+    
+    def inorder(self, root):
+        if root.left:
+            self.inorder(root.left)
+        self.k -= 1
+        if self.k == 0:
+            self.res = root.val
+            return
+        if root.right:
+            self.inorder(root.right)
+            
