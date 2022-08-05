ae85d2e15172fb264920319dc944434fef1ffc37
Vikram Bajaj <vikrambajaj@nyu.edu>
2022-08-04 20:43:57 -0400

    add submission

---
+++ 199_binary_tree_right_side_view.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # return last value in each level for a level-order traversal
+        res = []
+        if not root:
+            return
+        queue = [root]
+        
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(node.val)
+            
+        return res
